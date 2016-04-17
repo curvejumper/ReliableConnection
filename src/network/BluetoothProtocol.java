4588e05ea24569e5c80b4cce00433fffdc02a932
curvejumper <ortiz.d.michael@gmail.com>
2016-04-17 16:19:07 -0700

    Updated protocol, client and server for bluetooth

--- src/network/BluetoothProtocol.java
+++ src/network/BluetoothProtocol.java
@@ -60,7 +60,7 @@ public class BluetoothProtocol extends Protocol implements DiscoveryListener {
 //            PrintWriter pWriter = new PrintWriter(new OutputStreamWriter(outStream));
 //            pWriter.write(msg + "\r\n");
 //            pWriter.flush();
-            if(status()){
+            if (status()) {
                 notifyObservers(this);
             }
         } catch (IOException ex) {
@@ -81,81 +81,81 @@ public class BluetoothProtocol extends Protocol implements DiscoveryListener {
 
     @Override
     public Object connect() {
-        LocalDevice localDevice;
-        try {
-            localDevice = LocalDevice.getLocalDevice();
-            System.out.println("Address: " + localDevice.getBluetoothAddress());
-            System.out.println("Name: " + localDevice.getFriendlyName());
-
-            if (server) {
-                //Create the servicve url
-                String connectionString = "btspp://localhost:" + uuid + ";name=Sample SPP Server";
-
-                //open server url
-                StreamConnectionNotifier streamConnNotifier = (StreamConnectionNotifier) Connector.open(connectionString);
-
-                //Wait for client connection
-                System.out.println("\nServer Started. Waiting for clients to connect…");
-                connection = streamConnNotifier.acceptAndOpen();
-            } else {
-                //find devices
-                DiscoveryAgent agent = localDevice.getDiscoveryAgent();
-
-                System.out.println("Starting device inquiry…");
-                agent.startInquiry(DiscoveryAgent.GIAC, this);
-
-                try {
-                    synchronized (lock) {
-                        lock.wait();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                System.out.println("Device Inquiry Completed. ");
-
-//print all devices in vecDevices
-                int deviceCount = vecDevices.size();
-
-                if (deviceCount <= 0) {
-                    System.out.println("No Devices Found .");
-                    System.exit(0);
-                } else { //print bluetooth device addresses and names in the format [ No. address (name) ] 
-                    System.out.println("Bluetooth Devices: ");
-                    for (int i = 0; i < deviceCount; i++) {
-                        RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(i);
-                        System.out.println((i + 1) + ". " + remoteDevice.getBluetoothAddress() + " (" + remoteDevice.getFriendlyName(true) + ")");
-                    }
-                }
-                System.out.print("Choose Device index: ");
-                BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
-                String chosenIndex = bReader.readLine();
-                int index = Integer.parseInt(chosenIndex.trim());
-                //check for spp service 
-                RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(index - 1);
-                UUID[] uuidSet = new UUID[1];
-                uuidSet[0] = new UUID("1101", true);
-                System.out.println("\nSearching for service...");
-                agent.searchServices(null, uuidSet, remoteDevice, this);
-                try {
-                    synchronized (lock) {
-                        lock.wait();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                if (connectionURL == null) {
-                    System.out.println("Device does not support Simple SPP Service.");
-                    System.exit(0);
-                } //connect to the server and getOutputStream a line of text 
-                connection = (StreamConnection) Connector.open(connectionURL);
-            }
-
-        } catch (BluetoothStateException ex) {
-            Logger.getLogger(BluetoothProtocol.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(BluetoothProtocol.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        LocalDevice localDevice;
+//        try {
+//            localDevice = LocalDevice.getLocalDevice();
+//            System.out.println("Address: " + localDevice.getBluetoothAddress());
+//            System.out.println("Name: " + localDevice.getFriendlyName());
+//
+////            if (server) {
+////                //Create the servicve url
+////                String connectionString = "btspp://localhost:" + uuid + ";name=Sample SPP Server";
+////
+////                //open server url
+////                StreamConnectionNotifier streamConnNotifier = (StreamConnectionNotifier) Connector.open(connectionString);
+////
+////                //Wait for client connection
+////                System.out.println("\nServer Started. Waiting for clients to connect…");
+////                connection = streamConnNotifier.acceptAndOpen();
+////            } else {
+//            //find devices
+//            DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+//
+//            System.out.println("Starting device inquiry…");
+//            agent.startInquiry(DiscoveryAgent.GIAC, this);
+//
+//            try {
+//                synchronized (lock) {
+//                    lock.wait();
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            System.out.println("Device Inquiry Completed. ");
+//
+////print all devices in vecDevices
+//            int deviceCount = vecDevices.size();
+//
+//            if (deviceCount <= 0) {
+//                System.out.println("No Devices Found .");
+//                System.exit(0);
+//            } else { //print bluetooth device addresses and names in the format [ No. address (name) ] 
+//                System.out.println("Bluetooth Devices: ");
+//                for (int i = 0; i < deviceCount; i++) {
+//                    RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(i);
+//                    System.out.println((i + 1) + ". " + remoteDevice.getBluetoothAddress() + " (" + remoteDevice.getFriendlyName(true) + ")");
+//                }
+//            }
+//            System.out.print("Choose Device index: ");
+//            BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+//            String chosenIndex = bReader.readLine();
+//            int index = Integer.parseInt(chosenIndex.trim());
+//            //check for spp service 
+//            RemoteDevice remoteDevice = (RemoteDevice) vecDevices.elementAt(index - 1);
+//            UUID[] uuidSet = new UUID[1];
+//            uuidSet[0] = new UUID("1101", true);
+//            System.out.println("\nSearching for service...");
+//            agent.searchServices(null, uuidSet, remoteDevice, this);
+//            try {
+//                synchronized (lock) {
+//                    lock.wait();
+//                }
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            if (connectionURL == null) {
+//                System.out.println("Device does not support Simple SPP Service.");
+//                System.exit(0);
+//            } //connect to the server and getOutputStream a line of text 
+//            connection = (StreamConnection) Connector.open(connectionURL);
+////            }
+//
+//        } catch (BluetoothStateException ex) {
+//            Logger.getLogger(BluetoothProtocol.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(BluetoothProtocol.class.getName()).log(Level.SEVERE, null, ex);
+//        }
         return connection;
     }
 
@@ -171,6 +171,38 @@ public class BluetoothProtocol extends Protocol implements DiscoveryListener {
         connect();
     }
 
+    public void connect(RemoteDevice remoteDevice) {
+        LocalDevice localDevice;
+        try {
+            localDevice = LocalDevice.getLocalDevice();
+            DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+            //check for spp service 
+            UUID[] uuidSet = new UUID[1];
+            uuidSet[0] = new UUID("1101", true);
+            System.out.println("\nSearching for service...");
+            agent.searchServices(null, uuidSet, remoteDevice, this);
+            try {
+                synchronized (lock) {
+                    lock.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (connectionURL == null) {
+                System.out.println("Device does not support Simple SPP Service.");
+                System.exit(0);
+            } //connect to the server and getOutputStream a line of text 
+            connection = (StreamConnection) Connector.open(connectionURL);
+//            }
+
+        } catch (BluetoothStateException ex) {
+            Logger.getLogger(BluetoothProtocol.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(BluetoothProtocol.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        connect();
+    }
+
     @Override
     public void Close() {
         try {
@@ -182,6 +214,7 @@ public class BluetoothProtocol extends Protocol implements DiscoveryListener {
         }
     }
 
+    @Override
     public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
 //add the device to the vector
         if (!vecDevices.contains(btDevice)) {
@@ -190,6 +223,7 @@ public class BluetoothProtocol extends Protocol implements DiscoveryListener {
     }
 //implement this method since services are not being discovered
 
+    @Override
     public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
         if (servRecord != null && servRecord.length > 0) {
             connectionURL = servRecord[0].getConnectionURL(0, false);
@@ -200,12 +234,14 @@ public class BluetoothProtocol extends Protocol implements DiscoveryListener {
     }
 
 //implement this method since services are not being discovered
+    @Override
     public void serviceSearchCompleted(int transID, int respCode) {
         synchronized (lock) {
             lock.notify();
         }
     }
 
+    @Override
     public void inquiryCompleted(int discType) {
         synchronized (lock) {
             lock.notify();
--- src/reliableconn/CapitalizeClient.java
+++ src/reliableconn/CapitalizeClient.java
@@ -31,54 +31,15 @@ import static java.lang.Thread.sleep;
 import static java.lang.Thread.sleep;
 import static java.lang.Thread.sleep;
 import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
-import static java.lang.Thread.sleep;
+import java.util.ArrayList;
+import java.util.Vector;
+import javax.bluetooth.BluetoothStateException;
+import javax.bluetooth.DeviceClass;
+import javax.bluetooth.DiscoveryAgent;
+import javax.bluetooth.DiscoveryListener;
+import javax.bluetooth.LocalDevice;
+import javax.bluetooth.RemoteDevice;
+import javax.bluetooth.ServiceRecord;
 import static java.lang.Thread.sleep;
 import static java.lang.Thread.sleep;
 import static java.lang.Thread.sleep;
@@ -89,22 +50,20 @@ import static java.lang.Thread.sleep;
 import static java.lang.Thread.sleep;
 
 /**
- * A simple Swing-based client for the chat server.  Graphically
- * it is a frame with a text field for entering messages and a
- * textarea to see the whole dialog.
+ * A simple Swing-based client for the chat server. Graphically it is a frame
+ * with a text field for entering messages and a textarea to see the whole
+ * dialog.
  *
- * The client follows the Chat Protocol which is as follows.
- * When the server sends "SUBMITNAME" the client replies with the
- * desired screen name.  The server will keep sending "SUBMITNAME"
- * requests as long as the client submits screen names that are
- * already in use.  When the server sends a line beginning
- * with "NAMEACCEPTED" the client is now allowed to start
- * sending the server arbitrary strings to be broadcast to all
- * chatters connected to the server.  When the server sends a
- * line beginning with "MESSAGE " then all characters following
- * this string should be displayed in its message area.
+ * The client follows the Chat Protocol which is as follows. When the server
+ * sends "SUBMITNAME" the client replies with the desired screen name. The
+ * server will keep sending "SUBMITNAME" requests as long as the client submits
+ * screen names that are already in use. When the server sends a line beginning
+ * with "NAMEACCEPTED" the client is now allowed to start sending the server
+ * arbitrary strings to be broadcast to all chatters connected to the server.
+ * When the server sends a line beginning with "MESSAGE " then all characters
+ * following this string should be displayed in its message area.
  */
-public class CapitalizeClient {
+public class CapitalizeClient implements DiscoveryListener {
 
     BufferedReader in;
     PrintWriter out;
@@ -113,13 +72,19 @@ public class CapitalizeClient {
     JTextField textField = new JTextField(40);
     JTextArea messageArea = new JTextArea(8, 40);
 
+    //object used for waiting
+    private static Object lock = new Object();
+
+//vector containing the devices discovered
+    private static ArrayList arrDevices = new ArrayList();
+    private static String connectionURL = null;
+
     /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Return in the
-     * listener sends the textfield contents to the server.  Note
-     * however that the textfield is initially NOT editable, and
-     * only becomes editable AFTER the client receives the NAMEACCEPTED
-     * message from the server.
+     * Constructs the client by laying out the GUI and registering a listener
+     * with the textfield so that pressing Return in the listener sends the
+     * textfield contents to the server. Note however that the textfield is
+     * initially NOT editable, and only becomes editable AFTER the client
+     * receives the NAMEACCEPTED message from the server.
      */
     public CapitalizeClient() {
 
@@ -134,8 +99,8 @@ public class CapitalizeClient {
         textField.addActionListener(new ActionListener() {
             /**
              * Responds to pressing the enter key in the textfield by sending
-             * the contents of the text field to the server.    Then clear
-             * the text area in preparation for the next message.
+             * the contents of the text field to the server. Then clear the text
+             * area in preparation for the next message.
              */
             public void actionPerformed(ActionEvent e) {
                 out.println(textField.getText());
@@ -146,14 +111,78 @@ public class CapitalizeClient {
     }
 
     /**
-     * Prompt for and return the address of the server.
+     * Prompt for and return the wifiAddress of the server.
      */
-    private String getServerAddress() {
+    private String getWifiServerAddress() {
         return JOptionPane.showInputDialog(
-            frame,
-            "Enter IP Address of the Server:",
-            "Welcome to the Chatter",
-            JOptionPane.QUESTION_MESSAGE);
+                frame,
+                "Enter IP Address of the Server:",
+                "Welcome to the Chatter",
+                JOptionPane.QUESTION_MESSAGE);
+    }
+
+    private RemoteDevice getBtServerAddress() {
+
+        LocalDevice localDevice;
+        try {
+            localDevice = LocalDevice.getLocalDevice();
+
+            System.out.println("Address: " + localDevice.getBluetoothAddress());
+            System.out.println("Name: " + localDevice.getFriendlyName());
+            //find devices
+            DiscoveryAgent agent = localDevice.getDiscoveryAgent();
+
+            System.out.println("Starting device inquiry…");
+            agent.startInquiry(DiscoveryAgent.GIAC, this);
+
+            try {
+                synchronized (lock) {
+                    lock.wait();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Device Inquiry Completed. ");
+
+        } catch (BluetoothStateException ex) {
+            Logger.getLogger(CapitalizeClient.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(CapitalizeClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //print all devices in arrDevices
+        int deviceCount = arrDevices.size();
+
+        if (deviceCount <= 0) {
+            System.out.println("No Devices Found .");
+            return null;
+        } else { //print bluetooth device addresses and names in the format [ No. address (name) ] 
+            System.out.println("Bluetooth Devices: ");
+            String[] btDeviceNames = new String[arrDevices.size()];
+            for(int i = 0; i < arrDevices.size(); i++){
+                RemoteDevice btDevice = (RemoteDevice) arrDevices.get(i);
+                try {
+                    btDeviceNames[i] = (i + 1) + ". " + btDevice.getBluetoothAddress() + " (" + btDevice.getFriendlyName(true) + ")";
+                } catch (IOException ex) {
+                    Logger.getLogger(CapitalizeClient.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            String btDevice = (String) JOptionPane.showInputDialog(frame,
+                    "Alternate Protocol\n"
+                    + "Choose bluetooth device:",
+                    "Customized Dialog",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    btDeviceNames,
+                    "");
+            for(int i = 0; i < btDeviceNames.length; i++){
+                if(btDeviceNames[i].equals(btDevice)){
+                    return (RemoteDevice) arrDevices.get(i);
+                }
+            }
+        }
+        return null;
     }
 
     /**
@@ -161,10 +190,10 @@ public class CapitalizeClient {
      */
     private String getName() {
         return JOptionPane.showInputDialog(
-            frame,
-            "Choose a screen name:",
-            "Screen name selection",
-            JOptionPane.PLAIN_MESSAGE);
+                frame,
+                "Choose a screen name:",
+                "Screen name selection",
+                JOptionPane.PLAIN_MESSAGE);
     }
 
     /**
@@ -174,23 +203,24 @@ public class CapitalizeClient {
 
         // Make connection and initialize streams
         System.out.println("Generating network");
-        
+
         network = new Network();
         System.out.println("Observe protocols");
-        
+
         BluetoothProtocol bP = new BluetoothProtocol();
         WifiProtocol wP = new WifiProtocol();
-        
+
         wP.addObserver(network);
         bP.addObserver(network);
-        
+
         System.out.println("Attemting to connect...");
-        String address = getServerAddress();
-        System.out.println("Address: " + address);
-        wP.connect(address, 8888);
-        bP.connect("1101", true);
+        String wifiAddress = getWifiServerAddress();
+        RemoteDevice btDevice = getBtServerAddress();
+        System.out.println("Address: " + wifiAddress);
+        wP.connect(wifiAddress, 8888);
+        bP.connect(btDevice);
         System.out.println("Connected to server!");
-        
+
         in = new BufferedReader(new InputStreamReader(network.getInputStream()));
         out = new PrintWriter(network.getOutputStream(), true);
 
@@ -217,4 +247,39 @@ public class CapitalizeClient {
         client.frame.setVisible(true);
         client.run();
     }
+
+    @Override
+    public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+//add the device to the vector
+        if (!arrDevices.contains(btDevice)) {
+            arrDevices.add(btDevice);
+        }
+    }
+//implement this method since services are not being discovered
+
+    @Override
+    public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+        if (servRecord != null && servRecord.length > 0) {
+            connectionURL = servRecord[0].getConnectionURL(0, false);
+        }
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+
+//implement this method since services are not being discovered
+    @Override
+    public void serviceSearchCompleted(int transID, int respCode) {
+        synchronized (lock) {
+            lock.notify();
+        }
+    }
+
+    @Override
+    public void inquiryCompleted(int discType) {
+        synchronized (lock) {
+            lock.notify();
+        }
+
+    }//end method
 }
